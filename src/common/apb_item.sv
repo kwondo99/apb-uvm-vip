c327ac0ed54dac4bd09a44fb850bb82e55ece5d0
dongo <ehddh2245@naver.com>
2026-08-05 08:41:19 +0900

    edit

--- src/common/apb_item.sv
+++ src/common/apb_item.sv
@@ -2,11 +2,14 @@ class apb_item extends uvm_sequence_item;
 
   rand bit [31:0] PADDR;
   bit             PSEL;
-  rand bit        PENABLE;
+  bit             PENABLE;
   rand bit        PWRITE;
   rand bit [31:0] PWDATA;
   rand bit [31:0] PRDATA;
   rand bit        PREADY;
+  rand bit [ 3:0] PSTRB;
+  rand bit        PSLVERR;
+
 
   `uvm_object_utils_begin(apb_item)
     `uvm_field_int(PADDR, UVM_DEFAULT)
@@ -16,6 +19,8 @@ class apb_item extends uvm_sequence_item;
     `uvm_field_int(PWDATA, UVM_DEFAULT)
     `uvm_field_int(PRDATA, UVM_DEFAULT)
     `uvm_field_int(PREADY, UVM_DEFAULT)
+    `uvm_field_int(PSTRB, UVM_DEFAULT)
+    `uvm_field_int(PSLVERR, UVM_DEFAULT)
   `uvm_object_utils_end
 
   function new(string name = "apb_item");
